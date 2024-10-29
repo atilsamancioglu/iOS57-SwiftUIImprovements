f1d4794607c67862a17f00eec1bf99fc16867a6b
Atil Samancioglu <samancioglu.atil@gmail.com>
2024-10-29 18:20:01 +0300

    animation, gauge, custom toolbars

---
+++ SwiftUIComponents/CustomToolbarExample.swift
@@ -0,0 +1,18 @@
+//
+//  CustomToolBarExample.swift
+//  SwiftUIComponents
+//
+//  Created by Atil Samancioglu on 29.10.2024.
+//
+
+import SwiftUI
+
+struct CustomToolBarExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomToolBarExample()
+}
