239d71563409c96a6d50b972d03f5fb89514dcd0
Atil Samancioglu <samancioglu.atil@gmail.com>
2024-10-29 18:27:53 +0300

    accessibility added

---
+++ SwiftUIComponents/AccessibilityExample.swift
@@ -0,0 +1,18 @@
+//
+//  AccessibilityExample.swift
+//  SwiftUIComponents
+//
+//  Created by Atil Samancioglu on 29.10.2024.
+//
+
+import SwiftUI
+
+struct AccessibilityExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccessibilityExample()
+}
