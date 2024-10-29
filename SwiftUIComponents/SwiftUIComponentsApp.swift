fe3916a9856f0be1e8013ea67404f9eb5ffabb64
Atil Samancioglu <samancioglu.atil@gmail.com>
2024-10-29 17:36:00 +0300

    Initial Commit

---
+++ SwiftUIComponents/SwiftUIComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComponentsApp.swift
+//  SwiftUIComponents
+//
+//  Created by Atil Samancioglu on 29.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
